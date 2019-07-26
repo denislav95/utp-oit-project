78281628c09bad27bb9d7c4dcda8a4ce73a9edba
Denislav Zotev <denislav.zotev@novarto.com>
2019-07-26 09:11:57 +0300

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,40 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
-var cleanCSS = require('gulp-clean-css');
+var gulp = require("gulp");
+var less = require("gulp-less");
+var browserSync = require("browser-sync").create();
+var header = require("gulp-header");
+var cleanCSS = require("gulp-clean-css");
 var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
-var pkg = require('./package.json');
+var uglify = require("gulp-uglify");
+var pkg = require("./package.json");
+
+console.log("test");
 
 // Set the banner content
-var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
-    ' */\n',
-    ''
-].join('');
+var banner = [
+  "/*!\n",
+  " * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n",
+  " * Copyright 2013-" + new Date().getFullYear(),
+  " <%= pkg.author %>\n",
+  " * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n",
+  " */\n",
+  ""
+].join("");
 
 // Run everything
-gulp.task('default');
+gulp.task("default");
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: ''
-        },
-    })
-})
+gulp.task("browserSync", function() {
+  browserSync.init({
+    server: {
+      baseDir: ""
+    }
+  });
+});
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync','js'], function() {
-    gulp.watch('js/*.js', ['js']);
-    // Reloads the browser whenever HTML or JS files change
-    gulp.watch('pages/*.html', browserSync.reload);
+gulp.task("dev", ["browserSync", "js"], function() {
+  gulp.watch("js/*.js", ["js"]);
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch("pages/*.html", browserSync.reload);
 });
--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -1,3 +1,6 @@
+console.log('test')
+
+
 $(function() {
     $('#side-menu').metisMenu();
 });
